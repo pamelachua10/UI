751bc0ee1fd971bb7e3ea5a4cbaaf9d974ecf4bb
pamelachua10 <pamelachua10@gmail.com>
2015-04-18 23:44:10 +0800

    Revert "Straight Ahead with JS"
    This reverts commit 1aaea6bd4c0c47d572d16c872db7217ddad95f58.

--- StraightAhead with JS/js/comment.js
+++
@@ -1,8 +0,0 @@
-function showcommentform() {  
-	var data="*Comments, Special Instructions, Change Needed, etc.
-	//Name:<input type='text' name='name'>
-	<br><textarea rows='5' cols='40'></textarea>";
-	//<br><input type='submit' value='comment'>";  
-  
-	document.getElementById('mylocation').innerHTML=data;  
- }  
