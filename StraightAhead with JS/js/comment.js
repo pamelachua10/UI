1aaea6bd4c0c47d572d16c872db7217ddad95f58
pamelachua10 <pamelachua10@gmail.com>
2015-04-18 23:41:47 +0800

    Straight Ahead with JS

---
+++ StraightAhead with JS/js/comment.js
@@ -0,0 +1,8 @@
+function showcommentform() {  
+	var data="*Comments, Special Instructions, Change Needed, etc.
+	//Name:<input type='text' name='name'>
+	<br><textarea rows='5' cols='40'></textarea>";
+	//<br><input type='submit' value='comment'>";  
+  
+	document.getElementById('mylocation').innerHTML=data;  
+ }  
